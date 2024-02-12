47b33578cb79790b9661b1f91430ae485fae4ef6
carolineh-csuf <carolineh@csu.fullerton.edu>
2024-02-12 10:23:44 -0800

    Download for macOS. chrome, ios, android logic working.

--- lib/widgets/download_section.dart
+++ lib/widgets/download_section.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
+//import 'package:path_provider/path_provider.dart';
+import 'dart:io' as io;
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 //import 'dart:html' as html;
+import 'package:universal_html/html.dart' as html;
+
+//import 'package:universal_html/html.dart';
 
 class DownloadSection extends StatelessWidget {
   const DownloadSection({super.key});
@@ -19,36 +23,18 @@ class DownloadSection extends StatelessWidget {
         String resumeUrl =
             'https://docs.google.com/document/d/1hQVuqa2BjCXL6UvfIfUallCC0C2Ua0oH5atKlKVp5AY/edit#heading=h.y7d3xdxnr44m.pdf';
 
-        // Trigger the download action
-        downloadResume(resumeUrl);
-
-        // Trigger the download action
-        // html.AnchorElement anchorElement = html.AnchorElement(href: resumeUrl)
-        //   ..setAttribute("download", "resume.pdf");
-        // anchorElement.click();
+        if (kIsWeb) {
+          // Trigger the download action
+          html.AnchorElement anchorElement = html.AnchorElement(href: resumeUrl)
+            ..setAttribute("download", "resume.pdf");
+          anchorElement.click();
+        } else {
+          downloadResume(resumeUrl);
+        }
       },
       child: Text('Download Resume'),
     );
   }
-
-  // Future<void> downloadResume(String downloadUrl) async {
-  //   try {
-  //     // Send HTTP GET request to download the file
-  //     var response = await http.get(Uri.parse(downloadUrl));
-
-  //     // Get the directory for saving the downloaded file
-  //     Directory directory = await getApplicationDocumentsDirectory();
-  //     String filePath = '${directory!.path}/resume.pdf';
-
-  //     // Write the response body to the file
-  //     File file = File(filePath);
-  //     await file.writeAsBytes(response.bodyBytes);
-
-  //     print('File downloaded successfully to: $filePath');
-  //   } catch (e) {
-  //     print('Error downloading file: $e');
-  //   }
-  // }
 
   Future<void> downloadResume(String downloadUrl) async {
     try {
@@ -61,7 +47,7 @@ class DownloadSection extends StatelessWidget {
       // Ensure that the directory path is not null
       if (directoryPath != null) {
         // Create a new file object in the selected directory path
-        File file = File('$directoryPath/resume.pdf');
+        io.File file = io.File('$directoryPath/resume.pdf');
 
         // Write the response body to the file
         await file.writeAsBytes(response.bodyBytes);
@@ -74,42 +60,4 @@ class DownloadSection extends StatelessWidget {
       print('Error downloading file: $e');
     }
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text('Download Resume'),
-  //     ),
-  //     body: Center(
-  //       child: ElevatedButton(
-  //         onPressed: () {
-  //           _downloadFile(downloadUrl, fileName);
-  //         },
-  //         child: Text('Download Resume'),
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // void _downloadFile(String downloadUrl, String fileName) async {
-  //   try {
-  //     // Send HTTP GET request to download the file
-  //     var response = await http.get(Uri.parse(downloadUrl));
-
-  //     // Create a blob object from the response body
-  //     final blob = html.Blob([response.bodyBytes]);
-
-  //     // Create a download link and trigger a click event to download the file
-  //     final url = html.Url.createObjectUrlFromBlob(blob);
-  //     final anchor = html.AnchorElement(href: url)
-  //       ..setAttribute('download', fileName)
-  //       ..click();
-
-  //     // Revoke the object URL to free up memory
-  //     html.Url.revokeObjectUrl(url);
-  //   } catch (e) {
-  //     print('Error downloading file: $e');
-  //   }
-  // }
 }
